120b48d4fe434b7e5a1cad86944a705eb3f0f40b
Omar Mohamed <omarmohamed@192.168.1.11>
2025-05-17 03:32:54 +0300

    Add specPattern to Cypress config to fix test discovery

--- cypress.config.js
+++ cypress.config.js
@@ -3,8 +3,9 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     baseUrl: "https://parabank.parasoft.com/parabank",
+    specPattern: "cypress/e2e/**/*.cy.js",  
     setupNodeEvents(on, config) {
-      // event listeners if needed
+     
     },
   },
 });
